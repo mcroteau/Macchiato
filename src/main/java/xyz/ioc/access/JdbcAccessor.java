9c2bb7615b2a7804eb86dd7f5704253a4a6de50d
Mike Croteau <croteau.mike@gmail.com>
2020-09-07 13:48:33 -0800

    working through data access layer

--- src/main/java/xyz/ioc/access/JdbcAccessor.java
+++ src/main/java/xyz/ioc/access/JdbcAccessor.java
@@ -2,22 +2,24 @@ package xyz.ioc.access;
 
 import io.github.mcroteau.resources.access.Accessor;
 
+import java.sql.Statement;
 import java.util.Set;
 
 public class JdbcAccessor implements Accessor {
 
     @Override
-    public String getPassword(String s) {
+    public String getPassword(String username) {
         return null;
     }
 
     @Override
-    public Set<String> getRoles(String s) {
+    public Set<String> getRoles(String username) {
+
         return null;
     }
 
     @Override
-    public Set<String> getPermissions(String s) {
+    public Set<String> getPermissions(String username) {
         return null;
     }
 }
--- src/main/java/xyz/ioc/dao/UserDao.java
+++ src/main/java/xyz/ioc/dao/UserDao.java
@@ -1,10 +1,71 @@
 package xyz.ioc.dao;
 
+import xyz.ioc.factory.DbFactory;
+import xyz.ioc.model.User;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
+
 public class UserDao {
 
+    Connection connection;
+
+    public UserDao(){
+        connection = DbFactory.getConnection();
+    }
+
+    public User getUser(String username){
+        try {
+            Statement stmt = connection.createStatement();
+            String sql = "select * from accounts where username = '" + username + "'";
+            ResultSet rs = stmt.executeQuery(sql);
+            if(rs.next()){
+                User user = new User();
+                user.setId(rs.getLong("id"));
+                user.setName(rs.getString("name"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                return user;
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
     public String getPassword(String username){
-        String sql = "select * from accounts where username = '" + username + "'";
-        return "";
+        try {
+            Statement stmt = connection.createStatement();
+            String sql = "select password from accounts where username = '" + username + "'";
+            ResultSet rs = stmt.executeQuery(sql);
+            if (rs.next()) {
+                return rs.getString("password");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Set<String> getUserRoles(String username){
+        try {
+            User user = getUser(username);
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("select r.name from account_roles ur, role r where ur.role_id = r.id and ur.account_id = " + user.getId());
+            Set roles = new HashSet();
+            while(rs.next()){
+                String role = rs.getString("name");
+                roles.add(role);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
