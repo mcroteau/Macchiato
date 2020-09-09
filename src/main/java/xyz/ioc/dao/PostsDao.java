637fc8b37429fb40b202af94812a7b7347de73ae
Mike Croteau <croteau.mike@gmail.com>
2020-09-09 09:51:43 -0800

    working on post dao

--- src/main/java/xyz/ioc/dao/PostsDao.java
+++ src/main/java/xyz/ioc/dao/PostsDao.java
@@ -2,11 +2,14 @@ package xyz.ioc.dao;
 
 import xyz.ioc.factory.DbFactory;
 import xyz.ioc.model.Post;
+import xyz.ioc.model.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PostsDao {
     Connection connection;
@@ -23,6 +26,7 @@ public class PostsDao {
             stmt.setString(1, post.getTitle());
             stmt.setString(2, post.getContent());
             stmt.setLong(3, post.getDateCreated());
+
             ResultSet rs = stmt.executeQuery(sql);
             int result = stmt.executeUpdate();
             if(result == 1) return true;
@@ -31,5 +35,29 @@ public class PostsDao {
             ex.printStackTrace();
         }
         return false;
+    }
+
+    public List<Post> getPosts(String username){
+        try {
+            Statement stmt = connection.createStatement();
+            String sql = "select * from posts";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            List<Post> posts = new ArrayList<Post>();
+            while(rs.next()){
+                Post post = new Post();
+                post.setId(rs.getLong("id"));
+                post.setTitle(rs.getString("title"));
+                post.setContent(rs.getString("content"));
+                post.setDateCreated(rs.getLong("date_created"));
+                posts.add(post);
+            }
+
+            return posts;
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
